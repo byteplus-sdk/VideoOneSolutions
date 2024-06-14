7531c7057b645a54db378eee196fb73872624399
liucheng <liucheng.bigorange@bytedance.com>
2024-06-14 17:40:51 +0800

    feat: delete some information

--- Client/iOS/Component/RTCTokenOnlineKit/TokenGenerator.m
+++ Client/iOS/Component/RTCTokenOnlineKit/TokenGenerator.m
@@ -40,33 +40,10 @@
     }];
 }
 
-#if ENV_ONLINE
 - (void)protocol:(RTCTokenProtocol *)protocol getRTMPAddr:(NSString *)taskID block:(void (^)(NSString * _Nonnull))block {
     if (block) {
         block(@"");
     }
 }
-#else
-- (void)protocol:(RTCTokenProtocol *)protocol getRTMPAddr:(NSString *)taskID block:(void (^)(NSString * _Nonnull))block {
-    NSTimeInterval timeStamp = [[NSDate date] timeIntervalSince1970] * 1000;
-    NSTimeInterval expire = timeStamp + 7200;
-    NSString *path = [NSString stringWithFormat:@"/rtc_test/%@", taskID];
-    NSString *authKey = @"Uq8e962ghCCY2pBUB9Me2Fwy";
-    NSString *keyStr = [NSString stringWithFormat:@"%@%@%.0f", path, authKey, expire];
-    
-    const char *cKeyStr = [keyStr UTF8String];
-    unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(cKeyStr, (CC_LONG)strlen(cKeyStr), result);
-    NSMutableString *sign = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
-    for (NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
-        [sign appendFormat:@"%02x",result[i]];
-    }
-    
-    NSString *addr = [NSString stringWithFormat:@"rtmp://fcdn-test-hl.uplive.ixigua.com/rtc_test/%@?sign=%@&expire=%.0f", taskID, sign, expire];
-    if (block) {
-        block(addr);
-    }
-}
-#endif
 
 @end
